a16c5caf35289f69792d8bc0d47b0d28db1c3938
yusufguntav <ysf.guntav@gmail.com>
2024-05-19 15:13:27 +0300

    count person

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import os
+os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = '1'
 import cv2
 import argparse
 from ultralytics import YOLO
 import supervision as sv
+import numpy as np
+
+
+ZONE_POLYGON = np.array([
+    [0,0],
+    [1,0],
+    [1,1],
+    [0,1]
+])
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Yolov8 Object Detection')
@@ -17,6 +28,13 @@ def parse_args() -> argparse.Namespace:
     )
     return parser.parse_args() 
 
+def getPersonCount(itemList):
+    personCount = 0
+    for item in itemList:
+        if item == 0:
+            personCount += 1
+    return personCount
+
 def main():
     args = parse_args()
     
@@ -31,14 +49,28 @@ def main():
         text_thickness=1,
         text_scale=1
     )
+    zone_polygon = (ZONE_POLYGON * np.array(args.webcam_resolution)).astype(int)
+    zone = sv.PolygonZone(zone_polygon,frame_resolution_wh=tuple(args.webcam_resolution))
+    zone_annotator = sv.PolygonZoneAnnotator(zone,color=sv.Color.blue())
     
     while True:
         ret, frame = webcam.read()
-                
-        result = model(frame)[0]
+        
+        result = model(frame,device="mps")[0] 
+        
         detections = sv.Detections.from_yolov8(result)
         
-        frame = box_annotator.annotate(scene=frame, detections=detections)
+        labels = [
+            f"{model.model.names[class_id]}: {confidince:.2f}"
+            for _, confidince, class_id, _ in detections
+        ]
+             
+        frame = box_annotator.annotate(scene=frame, detections=detections,labels=labels)
+        
+        zone.trigger(detections)
+        frame = zone_annotator.annotate(scene=frame)
+        
+        cv2.putText(frame, str(getPersonCount(detections.class_id)),(10,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2) 
         
         cv2.imshow('yolov8', frame)
 
